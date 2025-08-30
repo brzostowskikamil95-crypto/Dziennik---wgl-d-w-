26737f709bab969187eed710b94b95e73a4f1ec3
brzostowskikamil95-crypto <brzostowskikamil95@gmail.com>
2025-08-31 01:22:44 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Dziennik Wglądów</h1>
+      <p>Witaj w aplikacji PWA!</p>
+    </div>
+  );
+}
+
+export default App;
